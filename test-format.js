5421f817f57068214d2a34302225bf3a38f2d5ee
lcw <xinglianyu@gmail.com>
2025-07-26 16:33:04 +0800

    test: 测试 pre-commit hook

---
+++ test-format.js
@@ -0,0 +1,15 @@
+// 这是一个测试文件，用于测试 pre-commit hook
+const test = {
+  name: 'test',
+  value: 123,
+  array: [1, 2, 3],
+  nested: {
+    key: 'value',
+  },
+}
+
+function testFunction() {
+  console.log('测试函数')
+}
+
+export { test, testFunction }
